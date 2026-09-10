7a02b53fa152f8b18e409d391ce397f07c3b5040
edgar.aesantos <edgaraesantos@gmail.com>
2026-09-10 14:44:38 -0300

    configurações na tela_add_ciente

--- Cadastro_Clinico/Connection.cs
+++ Cadastro_Clinico/Connection.cs
@@ -8,6 +8,7 @@ namespace Cadastro_Clinico
 {
     internal class Connection
     {
+        private readonly string connectionString =@"Data Source=VPR0681554W11-1\SQLEXPRESS;Initial Catalog=Projeto;User ID=sa;Password=123456;";
 
         public Connection() { }
 
